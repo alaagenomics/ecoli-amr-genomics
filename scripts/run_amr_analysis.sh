869e8c8393499fff6c12510dd79edc4e127b7046
alaagenomics <alaaabuzeeni@gmail.com>
2026-09-16 07:05:26 +0300

    Add K-12 MG1655 analysis to AMRFinderPlus script
    Added analysis for E. coli K-12 MG1655 alongside existing analysis for AR Bank #0346.

--- scripts/run_amr_analysis.sh
+++ scripts/run_amr_analysis.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# AMRFinderPlus analysis of E. coli AR Bank #0346
-INPUT="data/raw/ar0346/ar0346.fna"
-OUTPUT="results/amr/ar0346_amrfinder.tsv"
-amrfinder -n "$INPUT" -o "$OUTPUT"
+
+# AMRFinderPlus analysis of E. coli K-12 MG1655 and AR Bank #0346
+
+set -e
+
+amrfinder \
+  -n data/raw/GCF_000005845.2_ASM584v2_genomic.fna \
+  -o results/amr/k12_amrfinder.tsv
+
+amrfinder \
+  -n data/raw/ar0346/ar0346.fna \
+  -o results/amr/ar0346_amrfinder.tsv
